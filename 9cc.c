e5cc44f12549e5c1f837d9134db8f60faa478866
arato <arato.make@gmail.com>
2020-11-20 05:22:55 +0000

    単項プラスと単項マイナスに対応

--- 9cc.c
+++ 9cc.c
@@ -141,6 +141,7 @@ Node *new_node_num(int val) {
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
 
 Node *expr() {
     Node *node = mul();
@@ -156,13 +157,13 @@ Node *expr() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
@@ -176,6 +177,14 @@ Node *primary() {
     }
 
     return new_node_num(expect_number());
+}
+
+Node *unary() {
+    if (consume('+')) 
+        return unary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), unary());
+    return primary();
 }
 
 void gen(Node *node) {
--- test.sh
+++ test.sh
@@ -23,5 +23,8 @@ assert 41 " 12 + 34 - 5"
 assert 47 '5+6*7'
 assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
+assert 5 '6 + -1'
+assert 1 '- - 1'
+assert 1 '-5 + 6'
 
 echo OK
